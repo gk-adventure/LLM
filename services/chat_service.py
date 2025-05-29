a497eefae839275e73a55cf64f0c409216e4029f
Soojin6943 <sjhan0711@gmail.com>
2025-05-29 10:01:02 +0900

    feat: 수입(0),  수출(1)  타입 구분

--- services/chat_service.py
+++ services/chat_service.py
@@ -16,10 +16,14 @@ tools = [{
         "parameters": {
             "type": "object",
             "properties": {
+                "save_type": {
+                    "type": "integer",
+                    "description": "소비 내역이 수입이면 0, 지출이면 1 반환"
+                },
                 "category": {
                     "type": "string",
-                    "enum": ["카페", "식비", "교통", "의류", "문화", "공과금", "기타"],
-                    "description": "정해진 소비 카테고리 중 하나"
+                    "enum": ["카페", "식비", "교통", "의류", "문화", "공과금", "기타", "월급", "용돈"],
+                    "description": "정해진 카테고리 중 하나 수입이면 '월급', '용돈', '기타'에서 선택, 지출이면 '카페', '식비', '교통', '의류', '문화', '공과금', '기타'에서 선택"
                 },
                 "amount": {
                     "type": "integer",
@@ -34,7 +38,7 @@ tools = [{
                     "description": "소비에 대한 메모"
                 }
             },
-            "required": ["category", "amount", "date"],
+            "required": ["save_type", "category", "amount", "date"],
             "additionalProperties": False
         }
     },
@@ -54,7 +58,7 @@ def handle_message(message: str):
              너는 사용자 소비 내역을 정리해주는 가계부 어시스턴트야.
              오늘 날짜는 {today}야.
              만약 소비자가 '오늘', '어제', '그제'와 같은 말들을 하면 이를 기준으로 정확한 날짜(YYYY-MM-DD 형식)로 변환해서 바꿔줘.
-             소비 내역을 말하면 카테고리와 금액, 날짜, 메모를 파싱해서 JSON 형태로 반환해주고,
+             소비 내역을 말하면 수입인지 지출인지 판단하여 반환하고, 카테고리와 금액, 날짜, 메모를 파싱해서 JSON 형태로 반환해줘.
              일반적인 질문이나 조언(피드백) 요청이면 텍스트로 답변해줘."""},
              {"role": "user", "content": message}
         ],
@@ -71,6 +75,7 @@ def handle_message(message: str):
             "type": "save_expense",
             "reply": f"{args['category']} 카테고리로 {args['amount']}원을 기록했어요.",
             "data": {
+                "save_type": args["save_type"],
                 "category": args["category"], 
                 "amount": args["amount"],
                 "date": args["date"],
